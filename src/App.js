d43565d58b731fa76c1695e81934fa5ff6ec97ae
Rajesh J <rajesh.feb142002@gmail.com>
2024-07-04 14:50:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tasks: [],
-      count: 0,
+      tasks: [], //list that maintains all the tasks
+      count: 0,  //keeps a track of how many tasks are present
     };
   }
 
+  //retrieving the data that is saved in the local storage and updating the state accordingly
   componentDidMount() {
     const storedTasks = localStorage.getItem('todoList');
     if (storedTasks) {
@@ -22,6 +23,7 @@ class App extends Component {
     }
   }
 
+  //handles adding of task to the tasks list
   handleAddTask = (task) => {
     this.setState((prev) => ({
       tasks: [...this.state.tasks, task],
@@ -29,6 +31,7 @@ class App extends Component {
     }));
   }
 
+  //filters out the deleted list items and shows the undeleted ones
   handleDeleteTask = (index) => {
     this.setState((prev) => ({
       tasks: this.state.tasks.filter((task, i) => i !== index),
@@ -37,12 +40,14 @@ class App extends Component {
     localStorage.setItem("todoList", JSON.stringify(this.state.tasks.filter((task, i) => i !== index)));
   }
 
+  //updates the task when edited
   handleEditTask = (index, task) => {
     this.setState({
       tasks: this.state.tasks.map((t, i) => i === index ? task : t)
     });
   }
 
+  //saving to the localstorage so that, even when the page is reloded the data would'nt be lost
   savetToLocalStorage = () => {
     localStorage.setItem("todoList", JSON.stringify(this.state.tasks));
   }
@@ -53,12 +58,15 @@ class App extends Component {
       <div className='todo-container'>
         <h1>Todo App</h1>
         <div className='inputform'>
+          //Component TaskInput is beign called and a function is beign sent to it as a prop
           <TaskInput onAddTask={this.handleAddTask} />
         </div>
         <p>Total Tasks: {count}</p>
         <div className='task-list'>
+          //Component TaskList is beign called to get all the list items of tasks displayed with added functional buttons edit and delete
           <TaskList tasks={this.state.tasks} onDeleteTask={this.handleDeleteTask} onEditTask={this.handleEditTask} />
         </div>
+        //when clicked the data will be stored to the local storage
         <button className='addtask' onClick={this.savetToLocalStorage}>Save</button>
       </div>
     );
